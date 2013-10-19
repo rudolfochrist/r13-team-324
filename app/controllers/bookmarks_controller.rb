0bdf41808888ed32850c8b7b2baa52fe3945cad1
Verena Lambrecht <verena.lambrecht@gmail.com>
2013-10-19 12:28:06 +0200

    Add bookmark model with static content

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,20 @@
 class BookmarksController < ApplicationController
   def index
+  	m1 = Bookmark.new
+  	m1.url="elevensongs.com"
+  	m1.title = "Eleven Songs"
+  	m1.description = "The eleven most annoying songs in the world"
+
+  	m2 = Bookmark.new
+  	m2.url = "mtb.edu"
+  	m2.title = "Math Gym"
+  	m2.description = "Math courses and training"
+
+  	m3 = Bookmark.new
+  	m3.url = "http://www.google.de"
+  	m3.title = "Test"
+  	m3.description = "Test the best"
+
+  	@bookmarks = [m1,m2,m3]
   end
 end
---
+++ app/models/bookmark.rb
@@ -0,0 +1,3 @@
+class Bookmark
+	attr_accessor :title, :url, :description
+end
