24c104684d7d22564dcc7dbdb8e0c3d54c5d3bb1
Sebastian Christ <rudolfo.christ@gmail.com>
2013-10-19 23:03:23 +0200

    Create new nodes in graph

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,10 @@
+require 'neo4j'
 require 'active_model'
-class Bookmark
-	extend ActiveModel::Naming
-	include ActiveModel::Conversion
 
-	attr_accessor :title, :url, :description
+class Bookmark
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+  include Neo4j::Node
+
+  attr_accessor :title, :url, :description
 end
---
+++ lib/neo4j/node.rb
@@ -0,0 +1,29 @@
+require 'neography'
+
+module Neo4j
+  module Node
+    def create_in_graph
+      root = Neography::Node.load(0)
+      self_node = Neography::Node.create(self.to_hash)
+      Neography::Relationship.create(:is_bookmarked, root, self_node)
+      root.outgoing(:is_bookmarked) << self_node
+    end
+
+    def to_hash
+      self.instance_variables.inject({}) do |hash, var|
+        hash[var.to_s.delete('@')] = self.instance_variable_get(var)
+        hash
+      end
+    end
+
+    module ClassMethods
+      def find
+        'found'
+      end
+    end
+
+    def self.included(base)
+      base.extend ClassMethods
+    end
+  end
+end
