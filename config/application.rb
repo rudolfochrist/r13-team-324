d15f56b3e4bd537e608020e76337f164cefce61a
Sebastian Christ <rudolfo.christ@gmail.com>
2013-10-19 21:40:19 +0200

    Initialize Neo4j adapter.

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,10 @@ module Oevis
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # neography adapter
+    def neo_adapter
+      @neo_adapter ||= Neography::Rest.new
+    end
   end
 end
